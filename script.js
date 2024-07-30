6303417f5c3b4c722adafc2acab46329963790e7
hariteja <hariteja.in@gmail.com>
2024-07-30 12:50:04 +0530

    updated

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const inp = document.querySelector("input");
 const btn = document.querySelector("button");
 const copy = document.querySelector(".display img");
+const length = document.querySelector(".pass-length input");
 
 btn.addEventListener("click", ()=>{
     inp.value = "";
-    inp.value += genpass();
+    if(length.value < 1){
+        length.value = 5;
+    }else if(length.value > 16){
+        length.value = 16;
+    }
+    let passLength = length.value;
+    inp.value += genpass(passLength);
 })
 
 copy.addEventListener("click", ()=>{
@@ -19,9 +26,9 @@ const special = "~`!@#$%^&*()_-+={[}]|\:;'<,>.?/";
 
 const passwordArray = [uppercase, lowercase, numbers, special];
 
-function genpass(){
+function genpass(length){
     let password = "";
-    for(let i=0; i<10; i++){
+    for(let i=0; i<length; i++){
         let passtype = passwordArray[Math.floor(Math.random()*passwordArray.length)];
         password += passtype[Math.floor(Math.random()*passtype.length)];
     }
